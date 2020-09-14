e9658426312ce5d49e56f876abb4441f9d4ca966
keerthika-31 <68803761+keerthika-31@users.noreply.github.com>
2020-09-14 04:46:53 -0700

    Add files via upload

---
+++ asssignment1java.java
@@ -0,0 +1,22 @@
+//accept 2 values from comment line and concat them
+int=10;
+System.out.println(hello + r);
+\\output=hello+10
+
+
+//accept no from cmd line and add
+                int a=10;
+		float b=10.5f;
+		System.out.println(a + b);
+//output=20.5
+
+//check odd or even
+
+
+//convete dollor to rupee
+float dollars;
+		typesofvars in  = new typesofvars (system.in);
+				System.out.println("enter dollars");
+		dollars = in.nextlong();
+		float rupess = dollars * 64;
+		System.out.println(rupess + "rupes");
